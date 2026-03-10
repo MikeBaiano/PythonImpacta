7dd11ebb243b79a445db29f889db7f19e5eaf9d4
Michael Queen <michael.queen@aluno.impacta.edu.br>
2026-03-10 09:17:08 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,20 +1,25 @@
-num_semana = int(input("Digite o número da semana: "))
+# num_semana = int(input("Digite o número da semana: "))
 
-if num_semana == 1:
-    print("Domingo")
-elif num_semana == 2:
-    print("Segunda-feira")
-elif num_semana == 3:
-    print("Terça-feira")
-elif num_semana == 4:
-    print("Quarta-feira")
-elif num_semana == 5:
-    print("Quinta-feira")
-elif num_semana == 6:
-    print("Sexta-feira")
-elif num_semana == 7:
-    print("Sábado")
-else:
-    print("Número inválido!")
+# if num_semana == 1:
+#     print("Domingo")
+# elif num_semana == 2:
+#     print("Segunda-feira")
+# elif num_semana == 3:
+#     print("Terça-feira")
+# elif num_semana == 4:
+#     print("Quarta-feira")
+# elif num_semana == 5:
+#     print("Quinta-feira")
+# elif num_semana == 6:
+#     print("Sexta-feira")
+# elif num_semana == 7:
+#     print("Sábado")
+# else:
+#     print("Número inválido!")
+
+nome = "Paula"
+idade = 25
+
+print("Olá, " + nome + "! Você tem " + str(idade) + " anos.")
 
 
