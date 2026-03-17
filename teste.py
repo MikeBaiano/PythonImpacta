32dd59bef8c98626fd1a1a65bdcb75aa5f8720f4
Michael Queen <michael.queen@aluno.impacta.edu.br>
2026-03-17 09:25:11 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,5 @@
+import math as mt
+
 # num_semana = int(input("Digite o número da semana: "))
 
 # if num_semana == 1:
@@ -17,9 +19,26 @@
 # else:
 #     print("Número inválido!")
 
-nome = "Paula"
-idade = 25
+# nome = "Paula"
+# idade = 25
 
-print("Olá, " + nome + "! Você tem " + str(idade) + " anos.")
+# print("Olá, " + nome + "! Você tem " + str(idade) + " anos.")
+
+# def soma(n1, n2):
+#     s = n1 + n2
+#     return s
+
+# print(soma(10, 20))
+
+def par_impar(n):
+    if n % 2 == 0:
+        return "Sim"
+    else:
+        return "Não"
 
 
+
+print(f'{10} é par? {par_impar(10)}')
+print(f'{11} é par? {par_impar(11)}')
+
+print(f'O valor de pi é {mt.pi:.200f}')
